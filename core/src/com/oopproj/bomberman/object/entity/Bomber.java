a4e914f06c325e965ee1e4dbc565b190f4571b40
Lppt12345 <aibiettoi19@gmail.com>
2021-11-30 19:30:13 +0700

    them bomb

--- core/src/com/oopproj/bomberman/object/entity/Bomber.java
+++ core/src/com/oopproj/bomberman/object/entity/Bomber.java
@@ -3,15 +3,44 @@ package com.oopproj.bomberman.object.entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.oopproj.bomberman.data.Assets;
 import com.oopproj.bomberman.data.Direction;
 import com.oopproj.bomberman.data.Map;
+import com.oopproj.bomberman.object.item.Bomb;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class Bomber extends Entity {
 
+    protected List<Bomb> bombList;
+    protected int bomRate = 1;
+    protected int speed = 200;
+//    protected int
+    private Assets assets;
     public Bomber(Texture texture, int numberOfFrame, float x, float y) {
         super(texture, numberOfFrame, x, y);
         movingSpeed = 200;
     }
+
+
+    public void placeBomB (float x , float y){
+        if (isAlive){
+            Bomb bomb = new Bomb(assets.get(Assets.BOMB) , x,y);
+            bombList.add(bomb);
+        }
+    }
+
+    /**
+     * Xoa bom neu no qua gio
+     */
+    public void destroyBomb(){
+        for (Iterator<Bomb> iter = bombList.iterator(); iter.hasNext(); ) {
+            Bomb bomb = iter.next();
+            if(bomb.getTimeToExplore() < 0) iter.remove();
+        }
+    }
+
 
     @Override
     public void move(Map map) {
